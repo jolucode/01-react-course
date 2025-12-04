5f4d04490ff5f83ada5f5efd64eee5057a321ff5
jolucode <jolubere.92@gmail.com>
2025-12-03 22:51:04 -0500

    25. Añadiendo Colors para Colores en la Terminal

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,10 +1,12 @@
 import mongoose from 'mongoose';
+import colors from 'colors';
 
+console.log(process.env.MONGO_URI)
 export const connectDB = async () => {
   try {
     const {connection} = await mongoose.connect(process.env.MONGO_URI);
     const url = `${connection.host}:${connection.port}`;
-    console.log('Database connected to:', url);
+    console.log(colors.magenta.bold(`Database connected to ${url}`));
   } catch (error) {
     console.error('Database connection error:', error.message);
     process.exit(1);
